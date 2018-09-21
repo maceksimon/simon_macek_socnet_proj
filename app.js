9c4703b0197d9fd8095f599c4e710d2ba78c4849
Simon Macek <maceksimonlmd@gmail.com>
2018-09-21 10:18:31 +0200

    second chance

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const app = express();
 
 app.get('/', (req, res) => {
 	res.send('works');
-})
+});
 
 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`server started on port ${port}`);
 });
