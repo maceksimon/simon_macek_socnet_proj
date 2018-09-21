7f58bb3596381812e001ad8c1450b41bc9c2d451
Simon Macek <maceksimonlmd@gmail.com>
2018-09-21 10:13:26 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+
+
+// ROUTES 
+
+app.get('/', (req, res) => {
+	res.send('works');
+})
+
+
+
+const port = process.env.port || 5000;
+app.listen(port, () => {
+	console.log(`server started on port ${port}`);
+});
